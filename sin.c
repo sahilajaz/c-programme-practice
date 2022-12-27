bdb0f241fdf045725036dc31ce4fa639bb06c996
sahil ajaz <sahilajazw@gmail.com>
2022-12-27 16:09:56 +0530

    No of notes min req

---
+++ sin.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    float degree ;
+    printf("enter the angle in degree");
+    scanf("%f" , &degree);
+    printf("sin(%f)" , degree);
+    return 0;
+}
